8c6af9c7b7f37611ddba1ee2556f6d4ef40951e8
Solomon Kwon <solomonckwon@gmail.com>
2023-08-31 14:55:59 -0700

    ading routes, models

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,38 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()
 
-//routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
+//middleware
+//any request that comes in, check if there is a body to req, passes it to the req object
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000')
+//routes
+app.get('/', (req, res) => {
+    res.json({mssg: "website"})
 })
+
+app.use('/api/workouts', workoutRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to db and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
